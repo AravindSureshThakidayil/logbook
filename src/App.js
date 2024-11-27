95bb4f6aedb00c8a1e8bf4891f2540376e2e16de
Helina Alex <helinatalex@gmail.com>
2024-11-27 20:21:25 +0530

    familiarised with react

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var current_date =new Date();
+  const names_months = ["January", "February", "March",
+    "April", "May", "June", "July", "August", "Sept", "Oct", "Nov", "Dec"
+  ];
+  var elapsed_time;
+  
+  const show_message = function () {
+    alert("Hello!")
+  };
+
+  const press_1 = function () {
+    elapsed_time = Date.now();
+  }
+
+  const press_2 = function () {
+    elapsed_time = (Date.now() - elapsed_time) / 1000;
+    alert(elapsed_time.toString())
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>logbook</h1>
+      <h2>{current_date.getDate()}{names_months[current_date.getMonth()]}{current_date.getFullYear()}</h2>
+
+      <button onClick = {press_1}>1</button> 
+      <button onClick = {press_2}>2</button>  
     </div>
   );
 }

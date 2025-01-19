deb76b247ed73755aa1d24e4af65f258a71e204e
Aravind Suresh Thakidayil <aravind.suresh@hotmail.com>
2025-01-19 13:50:10 +0530

    test

--- src/App.js
+++ src/App.js
@@ -71,16 +71,15 @@ function App() {
             if (result) setInput(result.text);
           }}
         /> */}
-        <BarcodeScanner options={{
+        <BarcodeScanner onCapture={(barcodes) => {
+            barcodes.forEach((barcode) => alert(barcode.rawValue))
+          }} options={{
           formats: ["code_128", "code_39", "code_93", "ean_13"],
           width: { min: 640, ideal: 640 },
           height: { min: 480, ideal: 640 },
           facingMode: {
             ideal: 'environment'
-          },
-          onCapture: (barcodes) => {
-            barcodes.forEach((barcode) => alert(barcode.rawValue))
-          }        
+          },        
       }}/>
         <h4>{input}</h4>
         <input
